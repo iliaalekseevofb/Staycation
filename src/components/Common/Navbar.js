23c979cc9312001fe7ca051d6634065a779edd99
Ilia Alekseev <iliaalekseevofb@gmail.com>
2022-08-19 04:21:11 +0500

    Navigation bar is partially done (not responsive, partially functional)

--- src/components/Common/Navbar.js
+++ src/components/Common/Navbar.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { logo } from '../../assets';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='w-full h-20 px-5 flex justify-center items-center border-b border-neutral-200'>
+      <div className='w-full max-w-screen-2xl flex justify-between items-center'>
+        <Link to='/'><img src={logo} alt='logo' /></Link>
+        <div className='w-[480px] flex justify-between items-center text-lg'>
+          <a href='#home' className='hover:text-main duration-200'>Home</a>
+          <a href='#categories' className='hover:text-main duration-200'>Categories</a>
+          <a href='#testimonials' className='hover:text-main duration-200'>Testimonials</a>
+          <a href='#agents' className='hover:text-main duration-200'>Agents</a>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: '#3252DF'
+      }
+    },
   },
   plugins: [],
 }
