86f9916d17bbc266b486a6ddba387e12e11d6d10
Ilia Alekseev <iliaalekseevofb@gmail.com>
2022-08-19 04:43:06 +0500

    Navigation bar is done (responsive, functional)

--- src/components/Common/Navbar.js
+++ src/components/Common/Navbar.js
@@ -1,17 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import { logo } from '../../assets';
+import { FiMenu } from 'react-icons/fi';
+
+const navbarContent = [
+  ['Home', '#home'],
+  ['Categories', '#categories'],
+  ['Testimonials', '#testimonials'],
+  ['Agents', '#agents']
+]
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+  const menuRef = useRef();
+  useEffect(() => {
+    let menuHandler = (e) => {
+      if (!menuRef.current.contains(e.target))
+      setToggleMenu(false);
+    };
+    document.addEventListener('mousedown', menuHandler)
+  });
+
   return (
     <div className='w-full h-20 px-5 flex justify-center items-center border-b border-neutral-200'>
       <div className='w-full max-w-screen-2xl flex justify-between items-center'>
         <Link to='/'><img src={logo} alt='logo' /></Link>
-        <div className='w-[480px] flex justify-between items-center text-lg'>
-          <a href='#home' className='hover:text-main duration-200'>Home</a>
-          <a href='#categories' className='hover:text-main duration-200'>Categories</a>
-          <a href='#testimonials' className='hover:text-main duration-200'>Testimonials</a>
-          <a href='#agents' className='hover:text-main duration-200'>Agents</a>
+        <div className='w-[480px] hidden md:flex justify-between items-center text-lg'>
+          {navbarContent.map((item, index) => (
+            <a href={item[1]} className='hover:text-main duration-200'>{item[0]}</a>
+          ))}
+        </div>
+        <div className='block md:hidden' ref={menuRef}>
+          <button onClick={() => setToggleMenu(!toggleMenu)}>
+            <FiMenu className='hover:text-main duration-200' size={32} />
+          </button>
+          <div className={`absolute z-30 ${toggleMenu ? 'block' : 'hidden'} top-20 left-0 right-0 flex flex-col justify-center items-start bg-neutral-100`}>
+            {navbarContent.map((item, index) => (
+              <a onClick={() => setToggleMenu(false)} href={item[1]} className='w-full px-5 py-3 hover:text-main duration-200 border-b border-neutral-200'>{item[0]}</a>
+            ))}
+          </div>
         </div>
       </div>
     </div>
