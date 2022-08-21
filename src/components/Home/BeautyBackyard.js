3d6736d618c34a7fb4580851312142d53102d939
Ilia Alekseev <iliaalekseevofb@gmail.com>
2022-08-22 01:07:06 +0500

    Most picked section is fixed

--- src/components/Home/BeautyBackyard.js
+++ src/components/Home/BeautyBackyard.js
@@ -14,7 +14,7 @@ const BeautyBackyard = () => {
     <div className='w-full px-5 sm:px-10 mt-10 2xl:mt-16 flex justify-center items-center'>
       <div className='w-full max-w-screen-2xl'>
         <h3 className='mb-5 text-secondary text-3xl font-medium'>Most Picked</h3>
-        <div className='grid grid-cols-4 grid-flow-row gap-6'>
+        <div className='grid grid-cols-4 grid-rows-1 grid-flow-row gap-6'>
           {options.map((item, index) => (
             <Link className='relative' to={`place${[index + 1].toString()}`}>
               <div className='w-full h-56 bg-black rounded-xl overflow-hidden'>
--- src/components/Home/MostPicked.js
+++ src/components/Home/MostPicked.js
@@ -12,12 +12,12 @@ const gridContent = [
 
 const MostPicked = () => {
   return (
-    <div id='categories' className='w-full px-5 sm:px-10 mt-10 2xl:mt-16 flex justify-center items-center'>
+    <div id='categories' className='w-full px-5 sm:px-10 pt-10 2xl:pt-16 flex justify-center items-center'>
       <div className='w-full max-w-screen-2xl'>
         <h3 className='mb-5 text-secondary text-3xl font-medium'>Most Picked</h3>
         <div className='grid grid-rows-5 sm:grid-rows-3 lg:grid-rows-2 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6'>
           {gridContent.map((item, index) => (
-            <div key={index} className={`relative ${index === 0 ? 'col-span-1 sm:col-span-2 lg:col-span-1 row-span-1 lg:row-span-2 rounded-xl overflow-hidden' : 'col-span-1 lg:row-span-1 h-full rounded-xl overflow-hidden'}`}>
+            <div key={index} className={`relative max-h-72 lg:max-h-full ${index === 0 ? 'col-span-1 sm:col-span-2 lg:col-span-1 row-span-1 lg:row-span-2 rounded-xl overflow-hidden' : 'col-span-1 lg:row-span-1 rounded-xl overflow-hidden'}`}>
               <Link to={`place${[index + 1].toString()}`}>
                 <img src={item[0]} className='relative z-20 w-full h-full object-cover rounded-xl brightness-90 transition ease-in-out duration-500 hover:scale-125' alt='...' />
                 <div className='absolute z-30 min-w-[180px] w-1/2 h-12 top-0 right-0 flex justify-center items-center rounded-tr-xl rounded-bl-xl bg-thirdly text-white text-lg'>
