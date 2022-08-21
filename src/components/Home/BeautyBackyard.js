4ca8579385ddd9affe809164337e4a7dca88948e
Ilia Alekseev <iliaalekseevofb@gmail.com>
2022-08-22 01:11:49 +0500

    Beauty backyards section is done (responsive and functional)

--- src/components/Home/BeautyBackyard.js
+++ src/components/Home/BeautyBackyard.js
@@ -13,10 +13,10 @@ const BeautyBackyard = () => {
   return (
     <div className='w-full px-5 sm:px-10 mt-10 2xl:mt-16 flex justify-center items-center'>
       <div className='w-full max-w-screen-2xl'>
-        <h3 className='mb-5 text-secondary text-3xl font-medium'>Most Picked</h3>
-        <div className='grid grid-cols-4 grid-rows-1 grid-flow-row gap-6'>
+        <h3 className='mb-5 text-secondary text-3xl font-medium'>Houses with beauty backyard</h3>
+        <div className='grid grid-cols-1 grid-flow-row sm:grid-cols-2 xl:grid-cols-4 sm:grid-rows-2 xl:grid-rows-1 gap-6'>
           {options.map((item, index) => (
-            <Link className='relative' to={`place${[index + 1].toString()}`}>
+            <Link key={index} className='relative' to={`place${[index + 1].toString()}`}>
               <div className='w-full h-56 bg-black rounded-xl overflow-hidden'>
                 <img className='w-full h-full object-cover transition ease-in-out duration-500 hover:scale-125' src={item[0]} alt={item[1]} />
               </div>
@@ -27,7 +27,7 @@ const BeautyBackyard = () => {
               {item?.[3] && 
                 <div className='absolute z-30 min-w-[180px] w-1/2 h-12 top-0 right-0 flex justify-center items-center rounded-tr-xl rounded-bl-xl bg-thirdly text-white text-lg'>
                   <p className='pr-1'>{item[3]}</p>
-              </div>
+                </div>
               }
             </Link>
           ))}
