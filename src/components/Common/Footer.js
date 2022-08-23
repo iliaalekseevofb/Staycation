10271b673e0cc3b8e72dcd816832687dc9f47900
Ilia Alekseev <iliaalekseevofb@gmail.com>
2022-08-24 02:29:38 +0500

    Some minor changes are made to the whole home page

--- src/components/Common/Footer.js
+++ src/components/Common/Footer.js
@@ -10,7 +10,7 @@ const footerContent = [
 
 const Footer = () => {
   return (
-    <div className='w-full px-5 sm:px-10 py-16 flex justify-center items-center border-t border-neutral-200'>
+    <div id='agents' className='w-full px-5 sm:px-10 py-16 flex justify-center items-center border-t border-neutral-200'>
       <div className='w-full max-w-screen-2xl flex flex-col justify-between items-center'>
         <div className='w-full grid grid-cols-4 grid-rows-1 gap-16 mb-16'>
           <div>
@@ -22,7 +22,7 @@ const Footer = () => {
               <h3 className='text-2xl font-medium text-secondary mb-4'>{item[0]}</h3>
               <div>
                 {item[1].map((innerItem, innerIndex) => (
-                  <h5 className='text-xl text-neutral-300 font-light py-1 cursor-pointer' key={innerIndex}>{innerItem}</h5>
+                  <h5 className='text-xl text-neutral-300 font-light my-3 cursor-pointer' key={innerIndex}>{innerItem}</h5>
                 ))}
               </div>
             </div>
