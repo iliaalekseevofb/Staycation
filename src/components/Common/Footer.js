c4811512a8e648172923c876b5727efb695e1af1
Ilia Alekseev <iliaalekseevofb@gmail.com>
2022-08-24 03:23:22 +0500

    Home page is done, footer is done (responsive)

--- src/components/Common/Footer.js
+++ src/components/Common/Footer.js
@@ -5,20 +5,20 @@ import { logo } from '../../assets';
 const footerContent = [
   ['For Beginners', ['New Account', 'Start Booking a Room', 'Use Payments']],
   ['Explore Us', ['Our Careers', 'Privacy', 'Terms & Conditions']],
-  ['Connect Us', ['support@staycation.id', '021 - 2208 - 1996', 'Staycation, Kemang, Jakarta']]
+  ['Connect Us', ['support@staycation', '021 - 2208 - 1996', 'Staycation, Kemang, Jakarta']]
 ]
 
 const Footer = () => {
   return (
-    <div id='agents' className='w-full px-5 sm:px-10 py-16 flex justify-center items-center border-t border-neutral-200'>
+    <div id='agents' className='w-full px-5 sm:px-10 py-8 sm:py-16 flex justify-center items-center border-t border-neutral-200'>
       <div className='w-full max-w-screen-2xl flex flex-col justify-between items-center'>
-        <div className='w-full grid grid-cols-4 grid-rows-1 gap-16 mb-16'>
-          <div>
+        <div className='w-full sm:grid grid-cols-2 lg:grid-cols-4 grid-rows-2 lg:grid-rows-1 gap-16 mb-8 sm:mb-16'>
+          <div className='mb-8 sm:mb-0'>
             <Link to='/'><img src={logo} alt='logo' /></Link>
             <h5 className='text-neutral-300 text-xl font-light mt-2'>We kaboom your beauty holiday instantly and memorable.</h5>
           </div>
           {footerContent.map((item, index) => (
-            <div key={index}>
+            <div key={index} className='mb-8 sm:mb-0'>
               <h3 className='text-2xl font-medium text-secondary mb-4'>{item[0]}</h3>
               <div>
                 {item[1].map((innerItem, innerIndex) => (
@@ -28,7 +28,7 @@ const Footer = () => {
             </div>
           ))}
         </div>
-        <h5 className='text-lg text-neutral-300 font-light'>Copyright 2019 • All rights reserved • Staycation</h5>
+        <h5 className='text-center text-lg text-neutral-300 font-light'>Copyright 2019 • All rights reserved • Staycation</h5>
       </div>
     </div>
   )
