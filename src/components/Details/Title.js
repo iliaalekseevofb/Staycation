9452b35369a72779077e3ea542049414d493eda4
Ilia Alekseev <iliaalekseevofb@gmail.com>
2022-08-25 23:26:13 +0500

    Scroll to top component is added, basic breadcrumbs are added

--- src/components/Details/Title.js
+++ src/components/Details/Title.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Title = () => {
+const Title = ({id}) => {
   return (
-    <div>Title</div>
+    <div className='w-full px-5 sm:px-10 pt-10 2xl:pt-16 flex justify-center items-center'>
+      <div className='w-full max-w-screen-2xl grid grid-cols-3 grid-rows-1 gap-6'>
+        <div>
+          <Link to='/'>Home</Link>
+          <span>/</span>
+          <Link to={`/place${id}`}>Place Details</Link>
+        </div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
   )
 }
 
--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { Title, Gallery, About, Booking, Treasures, Testimonials } from '../components';
 
 const Details = () => {
   const {id} = useParams(); // String
 
   return (
     <div>
-      
+      <Title id={id}/>
+      <Gallery />
+      <div>
+        <About />
+        <Booking />
+      </div>
+      <Treasures />
+      <Testimonials />
     </div>
   )
 }
