51e4a24c7773864cdf76448fbd2cb27b22e2cc4d
Ilia Alekseev <iliaalekseevofb@gmail.com>
2022-08-27 17:51:50 +0300

    Most picked section and categories section are changed

--- src/components/Home/Categories.js
+++ src/components/Home/Categories.js
@@ -5,10 +5,10 @@ import dataHome from '../../data/dataHome.json';
 const Categories = () => {
   return (
     <div id='categories' className='w-full px-5 sm:px-10 flex justify-center items-center'>
-      <div className='w-full max-w-screen-2xl'>
+      <div className='w-full max-w-screen-xl'>
         {dataHome[2].categories && dataHome[2].categories.map((item, index) => (
           <div key={index} className='w-full h-full mt-10 2xl:mt-16'>
-            <h3 className='mb-5 text-secondary text-3xl font-medium'>{item.title}</h3>
+            <h3 className='mb-5 text-secondary text-2xl font-medium'>{item.title}</h3>
             <div className='grid grid-cols-1 grid-flow-row sm:grid-cols-2 xl:grid-cols-4 sm:grid-rows-2 xl:grid-rows-1 gap-6'>
               {item.cards.map((card, innerIndex) => (
                 <Link key={innerIndex} className='relative' to={`/place${card.id.toString()}`}>
@@ -16,11 +16,11 @@ const Categories = () => {
                     <img className='w-full h-full object-cover transition ease-in-out duration-500 hover:scale-125' src={card.img} alt={card.title} />
                   </div>
                   <div className='pt-4'>
-                    <h3 className='text-2xl text-secondary'>{card.title}</h3>
-                    <h5 className='text-lg font-light text-neutral-300'>{card.location}</h5>
+                    <h3 className='text-xl text-secondary'>{card.title}</h3>
+                    <h5 className='text-md font-light text-neutral-300'>{card.location}</h5>
                   </div>
                   {card.popularChoice === 1 && 
-                    <div className='absolute z-30 min-w-[180px] w-1/2 h-12 top-0 right-0 flex justify-center items-center rounded-tr-xl rounded-bl-xl bg-thirdly text-white text-lg'>
+                    <div className='absolute z-30 min-w-[180px] w-1/2 h-12 top-0 right-0 flex justify-center items-center rounded-tr-xl rounded-bl-xl bg-thirdly text-white text-md'>
                       <p className='pr-1'>Popular Choice</p>
                     </div>
                   }
--- src/components/Home/MostPicked.js
+++ src/components/Home/MostPicked.js
@@ -5,21 +5,21 @@ import dataHome from '../../data/dataHome.json';
 const MostPicked = () => {
   return (
     <div className='w-full px-5 sm:px-10 pt-10 2xl:pt-16 flex justify-center items-center'>
-      <div className='w-full max-w-screen-2xl'>
-        <h3 className='mb-5 text-secondary text-3xl font-medium'>Most Picked</h3>
+      <div className='w-full max-w-screen-xl'>
+        <h3 className='mb-5 text-secondary text-2xl font-medium'>Most Picked</h3>
         <div className='grid grid-rows-5 sm:grid-rows-3 lg:grid-rows-2 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6'>
           {dataHome[1].cards && dataHome[1].cards.map((item, index) => (
             <div key={index} className={`relative max-h-72 lg:max-h-full ${index === 0 ? 'col-span-1 sm:col-span-2 lg:col-span-1 row-span-1 lg:row-span-2 rounded-xl overflow-hidden' : 'col-span-1 lg:row-span-1 rounded-xl overflow-hidden'}`}>
               <Link to={`/place${item.id.toString()}`}>
                 <img src={item.img} className='relative z-20 w-full h-full object-cover rounded-xl brightness-90 transition ease-in-out duration-500 hover:scale-125' alt='...' />
-                <div className='absolute z-30 min-w-[180px] w-1/2 h-12 top-0 right-0 flex justify-center items-center rounded-tr-xl rounded-bl-xl bg-thirdly text-white text-lg'>
+                <div className='absolute z-30 min-w-[180px] w-1/2 h-12 top-0 right-0 flex justify-center items-center rounded-tr-xl rounded-bl-xl bg-thirdly text-white text-md'>
                   <span>$</span>
                   <p className='pr-1'>{item.price}</p>
                   <span>per night</span>
                 </div>
                 <div className='absolute z-30 bottom-6 left-6'>
-                  <h3 className='text-2xl text-white'>{item.title}</h3>
-                  <h5 className='text-lg font-light text-neutral-200'>{item.location}</h5>
+                  <h3 className='text-xl text-white'>{item.title}</h3>
+                  <h5 className='text-md font-light text-neutral-200'>{item.location}</h5>
                 </div>
               </Link>
             </div>
